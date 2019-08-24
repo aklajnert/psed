2fdeeccec66defba57622d9a10e3d2d76babb557
aklajnert <m@aklajnert.pl>
2019-08-24 11:24:43 +0200

    Create logger class

---
+++ psed/logger.py
@@ -0,0 +1,13 @@
+class Logger:
+    """Logging utility"""
+    verbosity = 0
+
+    @classmethod
+    def log(cls, message: str, level: int = 0):
+        if cls.verbosity >= level:
+            print(message)
+
+
+    @classmethod
+    def set_verbosity(cls, verbose: int):
+        cls.verbosity = verbose
