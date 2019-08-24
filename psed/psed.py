2e2e0cdfa131699ddc9172388d5eabd75f3826e5
aklajnert <m@aklajnert.pl>
2019-08-24 11:44:06 +0200

    Prepared method to parse input path with tests

--- psed/psed.py
+++ psed/psed.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import glob
 import os
 import sys
 import typing
@@ -22,5 +23,28 @@ class Psed:
         self.in_place: bool = inplace
 
     def run(self):
+        input_ = self._get_input()
+
+    def _get_input(self) -> typing.List[str]:
         if not os.path.exists(self.input):
-            sys.exit(f"Input not found in path: '{self.input}'")
+            glob_input = glob.glob(self.input)
+            if glob_input:
+                Logger.log("Glob has matched following files:", 1)
+                for item in glob_input:
+                    Logger.log(f"\t- {item}", 1)
+                return glob_input
+            sys.exit(f"The input path doesn't exist: '{self.input}'")
+        elif os.path.isfile(self.input):
+            Logger.log(f"Found the input file: {self.input}")
+            return [self.input]
+        else:
+            matches = []
+            for root, dirnames, filenames in os.walk(self.input):
+                for filename in filenames:
+                    matches.append(os.path.join(root, filename))
+            if not matches:
+                sys.exit(f"Input directory: '{self.input}' contains no files.")
+            Logger.log(f"Found {len(matches)} files in '{self.input}' directory:")
+            for item in matches:
+                Logger.log(f"\t- {item}", 1)
+            return matches
--- tests/test_psed.py
+++ tests/test_psed.py
@@ -1,3 +1,5 @@
+import re
+
 from click.testing import CliRunner
 
 from psed import __main__
@@ -7,8 +9,8 @@ def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(__main__.main)
-    assert result.exit_code == 0
-    assert "psed.cli.main" in result.output
+    assert result.exit_code == 2
+    assert 'Missing option "-i" / "--input"' in result.output
     help_result = runner.invoke(__main__.main, ["--help"])
     assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+    assert re.search(r"--help\s+Show this message and exit", help_result.output)
